812b881c3911f05008db08eb328859f0d1e8eee8
hrchlhck <vpemfh7@protonmail.com>
2021-05-20 16:39:06 -0300

    Changed names and added project-wide paths

--- constants.py
+++ constants.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
-__all__ = ['BASES', 'DATASETS', 'MONTHS']
+__all__ = ['VIEWS', 'DATASETS', 'MONTHS', 'CSV_PATH', 'IMAGES_PATH', 'LOGS_PATH']
 
-BASES = ["NIGEL", "MOORE", "VIEGAS", "ORUNADA"]
+VIEWS = ["NIGEL", "MOORE", "VIEGAS", "ORUNADA"]
 DATASETS_PATH = Path("./outDayDataset/")
-DATASETS = {year: {base : DATASETS_PATH.joinpath(str(year)).joinpath(base) for base in BASES} 
+DATASETS = {year: {view : DATASETS_PATH.joinpath(str(year)).joinpath(view) for view in VIEWS} 
             for year in range(2010, 2017)}
 MONTHS_NAME = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 MONTHS = [(str(i)) if i >= 10 else f"0{i}" for i in range(1, 13)]
 MONTHS = list(zip(MONTHS, MONTHS_NAME))
+CSV_PATH = Path('results/')
+IMAGES_PATH = Path('images/')
+LOGS_PATH = Path('logs/')
+MODELS_PATH = Path('models/')
