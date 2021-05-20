f45f0e2bf37ae484fbb864423552786aa87a8e4e
hrchlhck <vpemfh7@protonmail.com>
2021-05-20 16:37:55 -0300

    Added timer decorator and functions to ensemble classifiers

--- utils.py
+++ utils.py
@@ -1,11 +1,22 @@
-from constants import DATASETS
+from constants import DATASETS, MODELS_PATH
 from pathlib import Path
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
+from time import perf_counter
+from functools import wraps
+from logging import Logger
 import pandas as pd
+import pickle
+import sys
 
+__all__ = ['get_files', 'get_X_y', 'mean_accuracy', 'std_accuracy', 'clf_predict', 'show_results', 'mean', 'timer']
 
-__all__ = ['get_files', 'get_X_y', 'mean_accuracy', 'std_accuracy', 'clf_predict', 'show_results', 'mean']
+def print_progress(actual, total, label=None):
+    msg = f"{actual}/{total} - {(actual / total * 100):.2f}%\r"
+    if label and isinstance(label, str):
+        msg = f"{label}\t{msg}"
+    sys.stdout.write(msg)
+    sys.stdout.flush()
 
 def get_files(year: int, base: str, month: str, ext="csv") -> list:
     """ Get files from a given year and month from a dataset in MAWI laab dataset """
@@ -56,7 +67,7 @@ def clf_predict(clf: object, files: list, logger) -> dict:
     """ Computes classifier metrics from a CSV file within MAWI lab dataset """
     results = dict()
     for file in files:
-        logger.debug(f"Testing file {file}")
+        logger.debug(f"Testing file {file} in {clf.__class__.__name__}")
         corrects = 0
         samples = 0
         fn = 0
@@ -131,9 +142,93 @@ def show_results(results: dict) -> dict:
     ret = {
         'precision': precision, 'recall': recall, 
         'fpr': fpr, 'fnr': fnr, 
-        'fp': fpr, 'tn': tn, 
+        'fp': fp, 'tn': tn, 
         'fn': fn, 'tp': tp, 
         'mean_accuracy': mean_acc, 'mean_std': mean_std
     }
     
     return ret
+
+
+def timer(logger=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = perf_counter()
+            ret = func(*args, **kwargs)
+            end = perf_counter()
+            msg = f"Function {func.__name__} took {end - start:.4f}s to complete with arguments {args} and kwargs {kwargs}"
+
+            if logger and isinstance(logger, Logger):
+                logger.info(msg)
+            else:
+                print(msg)
+            return ret
+        return wrapper
+    return decorator
+
+def save_model(model: object, model_name: str) -> None:
+    _model = pickle.dumps(model)
+
+    with open(MODELS_PATH.joinpath(model_name + ".model"), 'wb') as fd:
+        fd.write(_model)
+
+def load_model(model_name: str) -> object:
+    with open(MODELS_PATH.joinpath(model_name), 'rb') as fd:
+        return pickle.loads(fd.read())
+
+def ensemble_predict(classifiers: list, X: list):
+    predictions = [clf.predict(X)[0] for _, clf in classifiers]
+    votes_by_class = ((predictions[i], predictions.count(obj)) for i, obj in enumerate(predictions))
+    votes_by_class = sorted(votes_by_class, key=lambda x: x[1])
+    return {'class': votes_by_class[-1][0], 'votes': votes_by_class[-1][1]}
+
+def ensemble(classifiers: list, files: list, logger: Logger):
+    results = dict()
+
+    for file in files:
+        logger.debug(f"Testing file {file} in ensemble")
+        corrects = 0
+        samples = 0
+        fn = 0
+        fp = 0
+        tp = 0
+        tn = 0
+        X_new, y_new = get_X_y(file)
+
+        for i in range(len(X_new)):
+            pred = ensemble_predict(classifiers, [X_new[i]])
+            if pred is not None:
+                if pred['class'] == y_new[i]:
+                    corrects += 1
+                # False negative
+                if pred['class'] == 0 and y_new[i] == 1:
+                    fn += 1
+                # False positive
+                if pred['class'] == 1 and y_new[i] == 0:
+                    fp += 1
+                # True positive
+                if pred['class'] == 1 and y_new[i] == 1:
+                    tp += 1
+                # False negative
+                if pred['class'] == 0 and y_new[i] == 0:
+                    tn += 1
+            samples += 1
+            print_progress(samples, len(X_new), label=file.name)
+        
+        # FNR: False Negative Rate
+        # FPR: False Positive Rateshow_results
+        results[file.name] = {
+            'corrects': corrects, 
+            'samples': samples, 
+            'fp': fp,
+            'fn': fn,
+            'fnr': fn / (fn + tp),
+            'fpr': fp / (fp + tn),
+            'tp': tp,
+            'tn': tn,
+            'recall': tp / (tp + fn),
+            'precision': tp / (tp + fp),
+            'accuracy': round(corrects / samples, 5),
+        }
+    return results
