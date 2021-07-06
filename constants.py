7b4ddc910592ffcd309afc12b243bf2cd7cb6915
hrchlhck <vpemfh7@protonmail.com>
2021-07-05 23:52:02 -0300

    Refactor

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ DATASETS = {year: {view : DATASETS_PATH.joinpath(str(year)).joinpath(view) for v
             for year in range(2010, 2017)}
 MONTHS_NAME = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 MONTHS = [(str(i)) if i >= 10 else f"0{i}" for i in range(1, 13)]
-MONTHS = list(zip(MONTHS, MONTHS_NAME))
+MONTHS = {month: name for month, name in zip(MONTHS, MONTHS_NAME)}
 CSV_PATH = Path('results/')
 IMAGES_PATH = Path('images/')
 LOGS_PATH = Path('logs/')
